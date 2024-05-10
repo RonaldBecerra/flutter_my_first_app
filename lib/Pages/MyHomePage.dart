1063258d546824417281fe8d6597be96e17d8cba
Ronald <12-10706@usb.ve>
2024-05-10 00:55:42 -0400

    Hasta la clase 4 del módulo 2

--- lib/Pages/MyHomePage.dart
+++ lib/Pages/MyHomePage.dart
@@ -23,20 +23,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pinkAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: SafeArea(
+        child: Text(
+        'You have pushed the button this many times:',
+      ),)
     );
   }
 }
