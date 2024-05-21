32a673c4188d7cdcf0f905fe80f77a8bd979e7f7
Ronald <12-10706@usb.ve>
2024-05-20 21:41:30 -0400

    Hasta la clase 7 del módulo 5. En el commit anterior no había puesto la imagen de shakira que no es la que está en un círculo

--- lib/Widgets/ListViewAndGridView/MyListView.dart
+++ lib/Widgets/ListViewAndGridView/MyListView.dart
@@ -4,7 +4,19 @@ import 'package:my_first_app/Widgets/ListViewAndGridView/Items/MyCard.dart';
 import 'package:my_first_app/Widgets/ListViewAndGridView/Items/MyContainer.dart';
 import 'package:my_first_app/Widgets/ListViewAndGridView/Items/MyListTile.dart';
 
-class MyListView extends StatelessWidget{
+class MyListView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState()=>MyListViewState();
+}
+
+class MyListViewState extends State<MyListView>{
+  List<Sound>mySounds = [];
+
+  @override
+  void initState() {
+    this.mySounds = sounds;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -16,8 +28,17 @@ class MyListView extends StatelessWidget{
         //physics: NeverScrollableScrollPhysics(),
         //scrollDirection: Axis.horizontal,
         itemCount: sounds.length,
-        itemBuilder: (context,index)=>MyListTile(sounds[index]),
-        //itemBuilder: (context,index)=>MyContainer(),
+        itemBuilder: (context,index){
+          Sound sound = this.mySounds[index];
+          return Dismissible(
+            direction: DismissDirection.startToEnd,
+            key: ObjectKey(sound),
+            child: MyListTile(sound),
+            onDismissed: (direction){
+              setState(()=>mySounds.remove(sound));
+            }
+          );
+        }, //itemBuilder: (context,index)=>MyContainer(),
       ),
     );
   }
